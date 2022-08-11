c8573fa2ed30eb4e8829ec3ca62d83048be1acff
skyv26 <dev.aakashv@gmail.com>
2022-08-12 00:19:15 +0530

    make file structure modular

--- index.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector('.button');
-
-button.addEventListener('click', (e) => {
-  alert('Hi, This is Aakash Verma');
-})
---
+++ module/heading.js
@@ -0,0 +1,10 @@
+
+
+const heading = () => {
+  const heading = document.createElement('h1');
+  heading.className='heading';
+  heading.textContent = `Hi! I've just started learning WEBPACK !!`;
+  return heading;
+}
+
+export default heading;
