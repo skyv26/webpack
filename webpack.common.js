5d75bf1f668b24ad07b8c820304b991e8029a083
skyv26 <dev.aakashv@gmail.com>
2022-08-12 10:49:40 +0530

    Split the webpack configuration

--- webpack.common.js
+++ webpack.common.js
@@ -2,13 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: "development",
-  devtool: false,
   entry: "./src/index.js",
-  output: {
-    filename: 'index.[contenthash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
   module: {
     rules: [
       {
